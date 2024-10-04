4fcba3f2e8a8b0349d3ee2b6093ab87607788a64
minnukota381 <minnukota381@gmail.com>
2024-10-04 12:00:00 +0530

    updated admin login ui

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
-  selector: 'app-admin',
+  selector: 'app-admin-login',
   templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent {
-  // Admin functionalities will be added here
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  login() {
+    this.userService
+      .login({ username: this.username, password: this.password })
+      .subscribe(
+        (response) => {
+          // Navigate to pond management page upon successful login
+          this.router.navigate(['/pond-management']);
+        },
+        (error) => {
+          // Handle error, like displaying incorrect credentials
+          this.errorMessage = 'Invalid login credentials';
+        }
+      );
+  }
 }
